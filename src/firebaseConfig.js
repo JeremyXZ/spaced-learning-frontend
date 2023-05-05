8eb820f8c340c3da7cfd3900273c4ac1bca26b2b
JeremyXZ <jeremyuk168@gmail.com>
2023-05-05 11:27:44 +0100

    correced errors in firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,15 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-    apiKey: REACT_APP_FIREBASE_API_KEY,
-    authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: REACT_APP_FIREBASE_PROJECTID,
-    storageBucket REACT_APP_FIREBASE_STORAGEBUCKET: ,
-    messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: REACT_APP_FIREBASE_APPID,
-    measurementId: REACT_APP_FIREBASE_MEASUREMENTID,
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: REACT_APP_FIREBASE_APPID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default firebaseApp;
-
