cbf9f35d87c325cad0d4c20334c172ef249b4163
JeremyXZ <jeremyuk168@gmail.com>
2023-05-05 11:23:10 +0100

    modified variable names in firebaseConfig file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,16 @@
 import firebase from "firebase/compat/app";
 
-const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG;
+const firebaseConfig = {
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECTID,
+    storageBucket REACT_APP_FIREBASE_STORAGEBUCKET: ,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: REACT_APP_FIREBASE_APPID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENTID,
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default firebaseApp;
+
